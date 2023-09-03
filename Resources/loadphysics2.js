e8ca6298a662604e4a8792dacea4388bb75e76ce
Muhammad Ubaid <39967022+muhammadubaid001@users.noreply.github.com>
2023-09-03 18:48:43 +0500

    Content updated

--- Resources/loadphysics2.js
+++ Resources/loadphysics2.js
@@ -1,92 +1,90 @@
 function load() {
-    // var div = document.getElementById("PhysicLoad");
-    var div2 = document.getElementById("PhysicLoad2");
-    // var div3 =document.getElementById("PhysicLoad3");
-    // var div4 =document.getElementById("PhysicLoad4");
+  // var div = document.getElementById("PhysicLoad");
+  var div2 = document.getElementById("PhysicLoad2");
+  // var div3 =document.getElementById("PhysicLoad3");
+  // var div4 =document.getElementById("PhysicLoad4");
 
-
-
-    let data = [
-        {
-            title: "Study resistors in a series circuit",
-            link: "../simulations/physics/VernierCalliper.html"
-        },
-        {
-            title: "Study Resistors in a parallax circuit",
-            link: "../simulations/physics/SimplePendulam2.html"
-        },
-        {
-            title: "Verify Ohm’s law (using wire as a conductor)",
-            link: "../simulations/physics/SimplePendulam.html"
-        },
-        {
-            title: "Find the focal length of a convex lens by parallax method",
-            link: "../simulations/physics/RefractionofLight.html"
-        },
-
-        {
-            title: "Verify the laws of refraction by using a glass slab",
-            link: "../simulations/physics/pricipleofmoment.html"
-        },
-        // {
-        //     title: `AC Sonometer`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Concave Mirror-Focal
-        //     Length by u-v Method`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Convex Mirror-Focal
-        //     Length`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Concave Lens-Focal Length`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Spectrometer-Prism`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Refractive Index of Liquid`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Metre bridge-Resistance
-        //     of a wire`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `MetreBridge-Law of
-        //     Combination of resistors`,
-        //     link: "ohm.html"
-        // }, {
-        //     title: `Diode Characteristics`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Zener Diode`,
-        //     link: "ohm.html"
-        // },
-        // {
-        //     title: `Transistor Characteristics`,
-        //     link: "ohm.html"
-        // }
-    ]
-    let HtmlText = "";
-    data.map((d, i) => {
-
-        HtmlText += `
+  let data = [
+    {
+      title: "Study resistors in a series circuit",
+      link: "../simulations/physics/VernierCalliper.html",
+    },
+    {
+      title: "Study Resistors in a parallax circuit",
+      link: "../simulations/physics/SimplePendulam2.html",
+    },
+    {
+      title: "Verify Ohm’s law (using wire as a conductor)",
+      link: "../simulations/physics/SimplePendulam.html",
+    },
+    {
+      title: "Find the focal length of a convex lens by parallax method",
+      link: "../simulations/physics/RefractionofLight.html",
+    },
+    {
+      title: "Verify the laws of refraction by using a glass slab",
+      link: "../simulations/physics/RefractionofLight.html",
+    },
+    // {
+    //     title: `AC Sonometer`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Concave Mirror-Focal
+    //     Length by u-v Method`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Convex Mirror-Focal
+    //     Length`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Concave Lens-Focal Length`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Spectrometer-Prism`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Refractive Index of Liquid`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Metre bridge-Resistance
+    //     of a wire`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `MetreBridge-Law of
+    //     Combination of resistors`,
+    //     link: "ohm.html"
+    // }, {
+    //     title: `Diode Characteristics`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Zener Diode`,
+    //     link: "ohm.html"
+    // },
+    // {
+    //     title: `Transistor Characteristics`,
+    //     link: "ohm.html"
+    // }
+  ];
+  let HtmlText = "";
+  data.map((d, i) => {
+    HtmlText += `
         <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 mb-4  mx-auto"
                         style="">
                         <div class="card" style="border:1px solid #71AF06; z-index:111;">
 
                             <img src="../pictures/physics/${i + 1}.png"
                                 class="card-img-top"
-                                style="width:100%; height: auto; object-fit:cover;" alt=" the experminent Picture of" +${d.title}>
+                                style="width:100%; height: auto; object-fit:cover;" alt=" the experminent Picture of" +${
+                                  d.title
+                                }>
 
                             <div class="card-body position-relative overflow-hidden" style="padding-bottom:65px;">
                                 <h1 class="card-title" style="font-size:
@@ -105,21 +103,22 @@ function load() {
                                         color:white; border-radius: 8px;
                                         font-weight: bold; position: absolute;
                                         bottom:0px; right:0px;">
-                                       <a href="${d.link}" style="text-decoration:none; color:white"> <i class="bi bi-arrow-right"></i> </a>
+                                       <a href="${
+                                         d.link
+                                       }" style="text-decoration:none; color:white"> <i class="bi bi-arrow-right"></i> </a>
                                     </span>
                                 </p>
                             </div>
 
                         </div>
                     </div>
-        `
-    })
+        `;
+  });
 
-    // div.innerHTML = HtmlText;
-    div2.innerHTML = HtmlText;
-    // div3.innerHTML = HtmlText;
-    // div4.innerHTML = HtmlText;
+  // div.innerHTML = HtmlText;
+  div2.innerHTML = HtmlText;
+  // div3.innerHTML = HtmlText;
+  // div4.innerHTML = HtmlText;
 }
 
-
-load()
+load();
